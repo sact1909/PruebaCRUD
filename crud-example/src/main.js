60689618d4c96cfc7a71cde95d48667e638fcfac
Steven Checo <steven.checo.19@gmail.com>
2020-09-02 19:15:48 -0400

    Models and Context Created

--- crud-example/src/main.js
+++ crud-example/src/main.js
@@ -1,8 +1,31 @@
 import Vue from 'vue'
-import App from './App.vue'
+//import App from './App.vue'
+import HelloWorld from './components/HelloWorld.vue'
+import MostrarDatos from './components/MostrarDatos.vue'
+const NotFound = { template: '<p>Page not found</p>' }
 
 Vue.config.productionTip = false
 
+const routes = {
+  '/': HelloWorld,
+  '/MostrarDatos': MostrarDatos
+}
+
+new Vue({
+  el: '#app',
+  data: {
+    currentRoute: window.location.pathname
+  },
+  computed: {
+    ViewComponent () {
+      return routes[this.currentRoute] || NotFound
+    }
+  },
+  render (h) { return h(this.ViewComponent) }
+})
+
+/*
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+*/
